8303b233dbe14252063df7263acd780d182e3c23
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2011-10-30 11:12:52 +0000

    Bit more o that lovely refactoring. AudioAvalable event seems to be outputting value not normalised between -1 and 1. This will cause problems! grrr!

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-function Instrument(sound_bank, mixer, trigger_bank, type) {
+var midiNoteFreq =  [ 16.35,    17.32,    18.35,    19.45,    20.6,     21.83,    23.12,    24.5,     25.96,    27.5,  29.14,    30.87,
+                      32.7,     34.65,    36.71,    38.89,    41.2,     43.65,    46.25,    49,       51.91,    55,    58.27,    61.74,
+                      65.41,    69.3,     73.42,    77.78,    82.41,    87.31,    92.5,     98,       103.83,   110,   116.54,   123.47,
+                      130.81,   138.59,   146.83,   155.56,   164.81,   174.61,   185,      196,      207.65,   220,   233.08,   246.94,
+                      261.63,   277.18,   293.66,   311.13,   329.63,   349.23,   369.99,   392,      415.3,    440,   466.16,   493.88,
+                      523.25,   554.37,   587.33,   622.25,   659.26,   698.46,   739.99,   783.99,   830.61,   880,   932.33,   987.77,
+                      1046.5,   1108.73,  1174.66,  1244.51,  1318.51,  1396.91,  1479.98,  1567.98,  1661.22,  1760,  1864.66,  1975.53,
+                      2093,     2217.46,  2349.32,  2489.02,  2637.02,  2793.83,  2959.96,  3135.96,  3322.44,  3520,  3729.31,  3951.07,
+                      4186.01,  4434.92,  4698.64,  4978 ];
+
+function Instrument(sound_bank, mixer, trigger_bank, type, printer) {
     
     var sound_source            = new SoundSource(sound_bank, type);
     var channel                 = new Channel(mixer, sound_source, type);
-    var meter_canvas_context    = channel.meter[0].getContext('2d');
     
     var trigger                 = $("<div class='trigger'><h3>" + type + "</h3></div>");
     $(trigger_bank).append(trigger)
@@ -49,10 +58,6 @@ function Instrument(sound_bank, mixer, trigger_bank, type) {
             soloOrUnsolo(solo);
         });
         
-        function amplitudeToPercentage(sample) {
-
-        }
-        
         function muteOrUnmute(button, sound_source, volume) {
             if (button.hasClass('mute')) {
                 sound_source.audio[0].volume = 0;
@@ -88,12 +93,12 @@ function Instrument(sound_bank, mixer, trigger_bank, type) {
 
         var channels;
         var rate;
-        var frameBufferLength;
+        var frame_buffer_length;
 
         function loadedMetadata() {
             channels                = self.audio[0].mozChannels;
             rate                    = self.audio[0].mozSampleRate;
-            frameBufferLength       = self.audio[0].mozFrameBufferLength;
+            frame_buffer_length     = self.audio[0].mozFrameBufferLength;
         }
         
         function audioAvailable(event) {   
@@ -103,17 +108,27 @@ function Instrument(sound_bank, mixer, trigger_bank, type) {
 
             // If stereo signal, then merge into mono signal and place into signal array above
             if (channels == 2) {
-                for (var i = 0, fbl = frameBufferLength / 2; i < fbl; i++ ) {
-                    signal[i] = (fb[2*i] + fb[2*i+1]) / 2; /* Load array with alternate amplitude values from each channel */
+                for (var i = 0, fbl = frame_buffer_length / 2; i < fbl; i++ ) {
+                    signal[i] = (fb[2*i] + fb[2*i+1]) / 2; /* Load array with average amplitude values from each channel */
                 }
             } else if (channels == 1) {
                 signal = fb /* Load array with original mono channel */
             }
             // iterate over the position of the playhead
-            for (var i = 0; i < frameBufferLength; i++) {
-                animateMeter(signal[i], meter_canvas_context);
-            }
+            for (var i = 0; i < frame_buffer_length; i++) {
+                animateMeter(signal[i]);
+             }
         }
+        
+        
+        
+        function writeSamples (event) {
+          lines.push (event.frameBuffer[0] + ', ' + event.frameBuffer[1] + ', ' + event.frameBuffer[2]);
+          printer.innerHTML = lines.join('<br>');
+        }
+        
+        
+        
         
         function play() {
             self.audio[0].currentTime = 0;
@@ -121,17 +136,32 @@ function Instrument(sound_bank, mixer, trigger_bank, type) {
         }
         this.play = play;
         
-        self.audio[0].addEventListener('MozAudioAvailable', audioAvailable, false);
+        var lines = [];
+        
+        self.audio[0].addEventListener('MozAudioAvailable', writeSamples, false);
         self.audio[0].addEventListener('loadedmetadata', loadedMetadata, false);
     }
     
+    
+    
     function valueToVolume(value) {
-      return (1/128)*value;
+        return (1/128)*value;
     }
     
-    function animateMeter(sample, context, meter) {
-      // context.clearRect(0, 0, meter.width, meter.height)
-      // var meter_value = amplitudeToPercentage(sample);
+    function animateMeter(sample) {
+        // var context = channel.meter[0].getContext('2d');
+        // context.clearRect(0, 0, channel.meter.width, channel.meter.height)
+        // var meter_value = (channel.meter.height()/100)*amplitudeAsPercentage(sample);
+        // debugger;
+    }
+    
+    function amplitudeAsPercentage(sample) {
+        if (sample < 0) {
+            /* Return the value as positive  */
+            return (0-sample)*100;  
+        } else {
+            return sample*100;
+        }
     }
 }
 
@@ -139,15 +169,15 @@ function Instrument(sound_bank, mixer, trigger_bank, type) {
 
 
 function init() {
+    var printer = document.getElementById("printer");
     var sound_bank      = document.getElementById("sound_bank");
     var mixer           = document.getElementById("mixer");
     var trigger_bank    = document.getElementById("trigger_bank");
     
-    
     var menu_items      = document.getElementById('options').children;
     
     $(menu_items).click(function() {
-      var instrument = new Instrument(sound_bank, mixer, trigger_bank, this.innerHTML);
+      var instrument = new Instrument(sound_bank, mixer, trigger_bank, this.innerHTML, printer);
     })
 }
 
