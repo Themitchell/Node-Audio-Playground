845766ea09e3dbf18e37a406f969b6e1e038f6cf
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2011-11-02 19:24:16 +0000

    hmm something funny here.. so i started using jasmine to try some test stuff (may try google-js-test) jasmine is nice though. Set timeout gets called and doesnt stop though. have i finally found a place for the after save?

--- javascripts/metro.js
+++ javascripts/metro.js
@@ -1,18 +1,17 @@
 function Metronome(visual_aid, tempo) {
     var self = this;
-    var counter;
+    self.counter = 0;
     var interval;
     var click = new Audio('samples/click.ogg');
     self.state = 0;
 
 
     function startClick() {
-        debugger;
         var spb     = 60/tempo; /* seconds per beat */
         var mspb    = 1000/spb; /* milliseconds per beat */
         
         /* Set counter to 0 */
-        counter = 0;
+        self.counter = 0;
         
         /* Set state to on (1) */
         self.state = 1;
@@ -36,7 +35,8 @@ function Metronome(visual_aid, tempo) {
         click.play();
         visual_aid.fadeIn(100);
         visual_aid.fadeOut(100);
-        counter++;
+        self.counter++;
     }
+    this.performClick = performClick;
     
 }
