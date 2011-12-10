fd3555b09921bff1741250808350fa4a41e5b8d9
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2011-12-10 21:25:52 +0000

    better nameing of functions

--- public/javascripts/mute.js
+++ public/javascripts/mute.js
@@ -2,31 +2,19 @@ function Mute(socket, sound_source, volume, type) {
   var self = this;
   this.toggle = new Toggle('internal', 'M', 'mute');
   
-  // function toggle(button, sound_source, volume) {
-  //     /*  TODO: Work out how to make mutes force a solo when exiting solos exist
-  //         var solo_buttons = $('.solo_button.unsolo');
-  //         if (solo_buttons >= 0) {
-  //             this.next().click();
-  //         }
-  //         This currently causes a loop whereby a solo no longer works as it uses
-  //         muteOrUnmute() to create a solo.
-  //     */
-  // 
-  //     if ($(button).hasClass('mute')) {
-  //         sound_source.audio[0].volume = 0;
-  //         $(button).addClass('unmute').removeClass('mute');
-  //     }
-  //     else if ($(button).hasClass('unmute')) {
-  //         sound_source.audio[0].volume = volume;
-  //         $(button).addClass('mute').removeClass('unmute');
-  //     }
-  // }
-  
-  function handleResponse() {
+  function handleToggle() {
+    //     /*  TODO: Work out how to make mutes force a solo when exiting solos exist
+    //         var solo_buttons = $('.solo_button.unsolo');
+    //         if (solo_buttons >= 0) {
+    //             this.next().click();
+    //         }
+    //         This currently causes a loop whereby a solo no longer works as it uses
+    //         muteOrUnmute() to create a solo.
+    //     */
     if (self.toggle.state == 0) {
       sound_source.audio[0].volume = 0;
     }
-    else if (self.toggle.state == 0) {
+    else if (self.toggle.state == 1) {
       sound_source.audio[0].volume = volume;
     }
     self.toggle.change_state();
@@ -37,6 +25,6 @@ function Mute(socket, sound_source, volume, type) {
   });
   
   socket.on('sendmutechannelinstrument', function(instrument_type) {
-    if (instrument_type == type) { handleResponse(); }
+    if (instrument_type == type) { handleToggle(); }
   });
 }
