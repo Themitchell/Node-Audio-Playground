7561bc2d5abaaf8a6862b8904bd650876dab35f8
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2011-12-18 18:52:11 +0000

    correctly muting instruments on load and clearing list when people leave:

--- config/sessions.js
+++
@@ -1,9 +0,0 @@
-var self = this;
-
-exports.all_entries = new Array();
-
-exports.remove_entry = function removeEntry(entry) {
-  for (var i=0; i<self.all_entries.length; i++) {
-    if (self.all_entries[i] == entry) { self.all_entries.splice(i, 1); }
-  }
-}
--- public/javascripts/toggle.js
+++ public/javascripts/toggle.js
@@ -1,29 +1,31 @@
-function Toggle(type, label, class_name_prefix) {
+function Toggle(init_state, type, label, class_name_prefix) {
   var self      = this;
+  this.state    = init_state;
   
-  this.state    = 0;
+  
   if (type == 'internal') {
-    this.button   = $("<div class='button " + class_name_prefix + " int_label off'>" + label + "</div>");
+    this.button   = $("<div class='button " + class_name_prefix + " int_label " + ((this.state == 1) ? 'on' : 'off') + "'>" + label + "</div>");
     this.wrapper  = this.button;
   }
   else {
-    this.button   = $("<div class='button off'>" + label + "</div>");
+    this.button   = $("<div class='button " + ((this.state == 1) ? 'on' : 'off') + "'>" + label + "</div>");
     this.wrapper  = $("<div class=\"toggle_wrapper " + class_name_prefix + " ext_label\"></div>");
     this.label    = $("<span></span>").text(label);
     this.wrapper.append(this.button);
   }
   
-  function changeState() {
+  function changeState(state) {
+    console.log("Changing toggle state from: " + self.state + " to: " + state);
+    
     if (self.state == 0) {
-      self.state = 1;
       self.button.addClass('on').removeClass('off');
     } 
     else if (self.state == 1) {
-      self.state = 0;
       self.button.addClass('off').removeClass('on');
     }
+    self.state = state;
+    console.log("Toggle state changed to: " + self.state);
     return self.state
   }
-  
   this.change_state = changeState;
 }
