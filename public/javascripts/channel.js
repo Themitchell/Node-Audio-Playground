23f12643f35a482feb2611a9ac34bb8ff7dd268f
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2011-12-22 00:34:15 +0000

    Adding equaliser and floating lightbox style windows for audio effects

--- public/javascripts/channel.js
+++ public/javascripts/channel.js
@@ -10,11 +10,13 @@ function Channel(socket, sound_source, current_identifier) {
   this.meter        = new Meter(sound_source, current_identifier);
   this.controls     = $("<div class='controls'></div>").append(this.volume.fader, this.meter.wrapper);
   
+  this.equaliser    = new Equaliser(socket, sound_source, current_identifier);
+  
   this.mute         = new Mute(socket, sound_source, this.volume.value, current_identifier);
   this.solo         = new Solo(socket, current_identifier);
-  this.buttons      = $("<div class='buttons'></div>").append(this.mute.toggle.button, this.solo.toggle.button);
+  this.buttons      = $("<div class='buttons'></div>").append(this.mute.toggle.button, this.solo.toggle.button, this.equaliser.toggle_window.button);
   
-  this.element.append(this.controls, this.buttons);
+  this.element.append(this.controls, this.buttons, this.equaliser.element);
   $(mixer).append(this.element);
   
 }
--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -5,3 +5,14 @@ function amplitudeAsPercentage(sample) {
 function valueToVolume(value) {
   return (1/128) * value;
 }
+
+function startOverlay() {
+	$("body").append('<div class="overlay"></div>').css({"overflow-y":"hidden"});
+	$(".overlay").animate({"opacity":"0.6"}, 200, 'linear');
+}
+
+function killOverlay() {
+  $(".overlay").animate({"opacity":"0"}, 200, 'linear', function(){
+		$(".overlay").remove();
+	})
+}
