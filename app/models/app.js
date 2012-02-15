b3811dbbf1e2b910adf8117a7bf19f37319d1277
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2012-02-15 13:09:28 +0000

    drop dems needless files

--- app/models/app.js
+++
@@ -1,9 +0,0 @@
-var AppModel = Backbone.Model.extend({
-    defaults: {
-        allowed_clients: 8
-    },
-
-    initialize: function() {
-        this.chats = new models.ChatCollection(); 
-    }
-});
